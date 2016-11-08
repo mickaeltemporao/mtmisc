a875164ed10a00fdc7bd5f125ad5a2f6bc0864e9
Mickael Temporão <mickael.temporao.1@ulaval.ca>
2016-11-08 15:35:42 -0500

    added na.rm to rmse fun

--- R/getRMSE.R
+++ R/getRMSE.R
@@ -5,14 +5,14 @@
 # Description:  TODO: (write me)
 # Version:      0.0.0.000
 # Created:      2016-11-08 15:23:27
-# Modified:     2016-11-08 15:30:04
+# Modified:     2016-11-08 15:35:28
 # Author:       Mickael Temporão < mickael.temporao.1 at ulaval.ca >
 # ------------------------------------------------------------------------------
 # Copyright (C) 2016 Mickael Temporão
 # Licensed under the GPL-2 < https://www.gnu.org/licenses/gpl-2.0.txt >
 # ------------------------------------------------------------------------------
 getRMSE <- function (y, y_hat, decimals=2) {
-  rmse <- sqrt(mean((y - y_hat)^2))
+  rmse <- sqrt(mean((y - y_hat)^2, na.rm=T))
   rmse <- round(rmse, decimals)
   return(rmse)
 }
